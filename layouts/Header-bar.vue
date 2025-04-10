36a64e39c92b9dafbf50dfa165d40e6de9236a8b
fatemeh nassari zadeh <nassarifa98@gmail.com>
2025-04-10 18:10:38 +0330

    add chnage ;ang to mobile view

--- layouts/Header-bar.vue
+++ layouts/Header-bar.vue
@@ -34,9 +34,9 @@
         </div>
         <!-- Mobile menu -->
         <div v-show="isMenuOpen" class="md:hidden mt-4 space-y-4">
-          <NuxtLink to="/" class="block hover:text-primary transition-colors" @click="isMenuOpen = false">Home</NuxtLink>
-          <NuxtLink to="/projects" class="block hover:text-primary transition-colors" @click="isMenuOpen = false">Projects</NuxtLink>
-          <NuxtLink to="/contact" class="block hover:text-primary transition-colors" @click="isMenuOpen = false">Contact</NuxtLink>
+          <NuxtLinkLocale to="/" class="block hover:text-primary transition-colors" @click="isMenuOpen = false">Home</NuxtLinkLocale>
+          <NuxtLinkLocale to="/projects" class="block hover:text-primary transition-colors" @click="isMenuOpen = false">Projects</NuxtLinkLocale>
+          <NuxtLinkLocale to="/contact-us" class="block hover:text-primary transition-colors" @click="isMenuOpen = false">Contact</NuxtLinkLocale>
           <button @click="$emit('theme-controller', !isDark)" class="p-2 rounded-lg hover:bg-primary/10 transition-colors">
             <svg v-if="isDark" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
@@ -44,6 +44,14 @@
             <svg v-else fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
             </svg>
+          </button>
+          <button class="relative" @click="isOpenDropdownTranslation = !isOpenDropdownTranslation">
+            <LanguageIcon class="w-5" />
+            <div v-motion-pop-visible v-if="isOpenDropdownTranslation" class="absolute top-8 shadow-lg w-max rounded-md md:right-0 bg-white">
+              <NuxtLink @click="changeLang(lang.code)" v-for="lang in languages" class="block py-2 px-4 transition-all hover:bg-slate-200 rounded-md">
+                  {{ lang.name }}
+              </NuxtLink>
+            </div>
           </button>
         </div>
       </div>
@@ -67,5 +75,6 @@ const changeLang = (code) => {
   const newPath = swichPath(code);
   navigateTo(newPath);
   store.enableLanguage = code;
+  isMenuOpen.value = false;
 }
 </script>
